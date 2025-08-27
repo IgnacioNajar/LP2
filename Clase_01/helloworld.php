2139bc52076c23c6f34542352fa154f43cff2b39
Ignacio Najar <ignacio.najar2310@gmail.com>
2025-08-27 19:49:39 -0300

    Agrega informacion personal y constantes al script PHP

--- Clase_01/helloworld.php
+++ Clase_01/helloworld.php
@@ -1,3 +1,12 @@
 <?php
-echo "Hola Mundo!";
+$nombre = "Ignacio";
+$apellido = "Najar";
+$edad = 27;
+define('FECHA_NAC', '23/10/97');
+define('MENSAJE_BIENVENIDA', 'Hola Mundo!');
+$menor_de_edad = False;
 ?>
+<h1><?= MENSAJE_BIENVENIDA; ?></h1>
+<p>Mi nombre es <?= $nombre; ?> <?= $apellido; ?>, y tengo <?= $edad; ?> años.</p>
+<p>Naci el <?= FECHA_NAC; ?></p>
+<p> Soy menor de edad? <?= $menor_de_edad; ?></p>
