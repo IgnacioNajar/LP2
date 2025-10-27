b9358b23aa458548b348e7b63e69719e45596bcd
Ignacio Najar <ignacio.najar2310@gmail.com>
2025-10-27 13:18:06 -0300

    Agrega archivos para realizar el segundo parcial

---
+++ Clase_11_Parcial/tdcsa.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-10-2025 a las 19:29:49
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tdcsa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `niveles`
+--
+
+CREATE TABLE `niveles` (
+  `Id` int(11) NOT NULL,
+  `Denominacion` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `niveles`
+--
+
+INSERT INTO `niveles` (`Id`, `Denominacion`) VALUES
+(1, 'Admin'),
+(2, 'Operador');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `transportes`
+--
+
+CREATE TABLE `transportes` (
+  `Marca` int(11) NOT NULL,
+  `Modelo` varchar(50) DEFAULT NULL,
+  `Disponible` varchar(50) DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `Id` int(11) NOT NULL,
+  `Nombre` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `niveles`
+--
+ALTER TABLE `niveles`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `niveles`
+--
+ALTER TABLE `niveles`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Clase_11_Parcial/testFechas.php
@@ -0,0 +1,45 @@
+<?php 
+/*esta funcion de PHP hace que al ejecutar este archivo, tome la hora de la ciudad mencionada */
+date_default_timezone_set("America/Argentina/Cordoba");
+//pueden usarla si lo desean en el trabajo para que calcule la fecha correctamente. 
+//la copian al principio del archivo donde trabajen con la fecha. 
+
+//analicen estos procesos para saber como trabajar en lo pedido:
+$Fecha_viaje="2025-11-02"; //esto será el valor que venga de la BD, pongan la fecha que quieran probar
+$Fecha_viaje="2025-11-03"; //esto será el valor que venga de la BD, pongan la fecha que quieran probar
+$Fecha_viaje="2025-10-20"; //esto será el valor que venga de la BD, pongan la fecha que quieran probar
+//$Fecha_viaje="2025-11-05"; //esto será el valor que venga de la BD, pongan la fecha que quieran probar
+
+//defino la fecha de hoy
+$Fecha_actual = date("Y-m-d"); 
+
+//de esta manera sabemos cual es la fecha de mañana (sumamos un dia a hoy)
+$Maniana= date("Y-m-d",strtotime($Fecha_actual."+ 1 day"));   
+
+//con ambos datos, podemos preguntar si la fecha del viaje es mañana?
+if ($Fecha_viaje == $Maniana){
+    echo "El viaje es mañana!  <br /> 
+    Fecha viaje:  $Fecha_viaje  / mañana será $Maniana <hr / >
+    Formato Español: Fecha viaje: ".date('d/m/Y',strtotime($Fecha_viaje))."  /  mañana será  ".date('d/m/Y',strtotime($Maniana)); 
+    echo "<hr / >";
+}else if ($Fecha_viaje > $Fecha_actual){
+//la fecha del viaje es mayor a hoy?
+
+    echo "El viaje despues de mañana, en adelante. <br /> Fecha viaje:  $Fecha_viaje  (hoy es $Fecha_actual)"; 
+}
+
+//la fecha del viaje es para hoy?
+if ($Fecha_viaje ==$Fecha_actual){
+    echo "El viaje es hoy! <br /> 
+    Fecha viaje:  $Fecha_viaje  / mañana será $Fecha_actual <hr / >
+    Formato Español: Fecha viaje: ".date('d/m/Y',strtotime($Fecha_viaje))."  /  mañana será  ".date('d/m/Y',strtotime($Fecha_actual)); 
+    echo "<hr / >";
+}
+//la fecha del viaje es menor a hoy?
+if ($Fecha_viaje < $Fecha_actual){
+    echo "El viaje ya se hizo. <br /> Fecha viaje:  $Fecha_viaje  (hoy es $Fecha_actual)"; 
+    
+}
+
+
+?>
