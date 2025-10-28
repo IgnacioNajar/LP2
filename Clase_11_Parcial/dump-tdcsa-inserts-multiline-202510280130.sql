dc0ffacba468789af13ec3bae165c96a1a2c55d2
Ignacio Najar <ignacio.najar2310@gmail.com>
2025-10-28 01:46:04 -0300

    Agrega archivos para la resolucion del parcial

---
+++ Clase_11_Parcial/dump-tdcsa-inserts-multiline-202510280130.sql
@@ -0,0 +1,215 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: tdcsa
+-- ------------------------------------------------------
+-- Server version	8.0.43
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `destino`
+--
+
+DROP TABLE IF EXISTS `destino`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `destino` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `denominacion` varchar(100) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `destino`
+--
+
+LOCK TABLES `destino` WRITE;
+/*!40000 ALTER TABLE `destino` DISABLE KEYS */;
+INSERT INTO `destino` VALUES (1,'Córdoba Capital');
+INSERT INTO `destino` VALUES (2,'Río Cuarto');
+INSERT INTO `destino` VALUES (3,'Villa María');
+INSERT INTO `destino` VALUES (4,'Villa Carlos Paz');
+INSERT INTO `destino` VALUES (5,'San Francisco');
+INSERT INTO `destino` VALUES (6,'Alta Gracia');
+INSERT INTO `destino` VALUES (7,'Río Tercero');
+INSERT INTO `destino` VALUES (8,'La Calera');
+INSERT INTO `destino` VALUES (9,'Bell Ville');
+INSERT INTO `destino` VALUES (10,'Villa Allende');
+INSERT INTO `destino` VALUES (11,'Villa Dolores');
+INSERT INTO `destino` VALUES (12,'Jesús María');
+INSERT INTO `destino` VALUES (13,'Cosquín');
+INSERT INTO `destino` VALUES (14,'La Falda');
+INSERT INTO `destino` VALUES (15,'Mina Clavero');
+/*!40000 ALTER TABLE `destino` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `marca`
+--
+
+DROP TABLE IF EXISTS `marca`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `marca` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `denominacion` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marca`
+--
+
+LOCK TABLES `marca` WRITE;
+/*!40000 ALTER TABLE `marca` DISABLE KEYS */;
+INSERT INTO `marca` VALUES (1,'Volkswagen');
+INSERT INTO `marca` VALUES (2,'Iveco');
+INSERT INTO `marca` VALUES (3,'Scania');
+INSERT INTO `marca` VALUES (4,'Volvo');
+/*!40000 ALTER TABLE `marca` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `nivel`
+--
+
+DROP TABLE IF EXISTS `nivel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `nivel` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `denominacion` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nivel`
+--
+
+LOCK TABLES `nivel` WRITE;
+/*!40000 ALTER TABLE `nivel` DISABLE KEYS */;
+INSERT INTO `nivel` VALUES (1,'Administrador');
+INSERT INTO `nivel` VALUES (2,'Operador');
+INSERT INTO `nivel` VALUES (3,'Chofer');
+/*!40000 ALTER TABLE `nivel` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transporte`
+--
+
+DROP TABLE IF EXISTS `transporte`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transporte` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `marcaId` int DEFAULT NULL,
+  `modelo` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `anio` smallint unsigned DEFAULT NULL,
+  `patente` varchar(7) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `disponible` tinyint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `transporte_marca_FK` (`marcaId`),
+  CONSTRAINT `transporte_marca_FK` FOREIGN KEY (`marcaId`) REFERENCES `marca` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transporte`
+--
+
+LOCK TABLES `transporte` WRITE;
+/*!40000 ALTER TABLE `transporte` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transporte` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `apellido` varchar(50) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `usuario` varchar(20) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `clave` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `activo` tinyint DEFAULT NULL,
+  `nivelId` int DEFAULT NULL,
+  `fechaCreacion` datetime DEFAULT NULL,
+  `imagen` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario_nivel_FK` (`nivelId`),
+  CONSTRAINT `usuario_nivel_FK` FOREIGN KEY (`nivelId`) REFERENCES `nivel` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `viaje`
+--
+
+DROP TABLE IF EXISTS `viaje`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `viaje` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `choferId` int DEFAULT NULL,
+  `transporteId` int DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `destinoId` int DEFAULT NULL,
+  `costoViaje` decimal(9,2) DEFAULT NULL,
+  `montoChofer` decimal(9,2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `viaje_usuario_FK` (`choferId`),
+  KEY `viaje_transporte_FK` (`transporteId`),
+  CONSTRAINT `viaje_transporte_FK` FOREIGN KEY (`transporteId`) REFERENCES `transporte` (`id`),
+  CONSTRAINT `viaje_usuario_FK` FOREIGN KEY (`choferId`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `viaje`
+--
+
+LOCK TABLES `viaje` WRITE;
+/*!40000 ALTER TABLE `viaje` DISABLE KEYS */;
+/*!40000 ALTER TABLE `viaje` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'tdcsa'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-10-28  1:30:06
---
+++ Clase_11_Parcial/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require_once('head.php'); ?>
+
+<body>
+
+    <!-- ======= Header ======= -->
+    <?php require_once('header.php'); ?>
+    <!-- End Header -->
+
+    <!-- ======= Sidebar ======= -->
+    <?php require_once('sidebar.php'); ?>
+    <!-- End Sidebar-->
+
+
+    <main id="main" class="main">
+
+        <div class="pagetitle">
+            <h1>Bienvenid@s!!</h1>
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item active"><a href="index.html">Home</a></li>
+                </ol>
+            </nav>
+        </div><!-- End Page Title -->
+
+        <section class="section dashboard">
+            <div class="row">
+
+                <!-- Left side columns -->
+                <div class="col-lg-12">
+                    <div class="row">
+
+                        <!-- Customers Card -->
+                        <div class="col-xxl-12 col-xl-12">
+
+                            <div class="card info-card customers-card">
+
+                                <div class="card-body">
+                                    <h5 class="card-title">Estamos trabajando en el segundo desempeño. <span>| Muchos exitos!</span></h5>
+                                    <div class="d-flex align-items-center">
+                                        <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-people"></i>
+                                        </div>
+                                        <div class="ps-3 text-danger small pt-1 fw-bold">
+                                            <p>Puedes seleccionar el menu de la izquierda.</p>
+                                            <p>Recuerda que para el Final o para Regularidad Extraordinaria, debes presentar el panel completo.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div><!-- End Customers Card -->
+
+                    </div>
+                </div><!-- End Left side columns -->
+
+            </div>
+        </section>
+
+    </main><!-- End #main -->
+
+
+
+    <!-- ======= Footer ======= -->
+    <?php require_once('footer.php'); ?>
+    <!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files
+    <script src="assets/vendor/apexcharts/apexcharts.min.js"></script> -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- <script src="assets/vendor/chart.js/chart.umd.js"></script>
+    <script src="assets/vendor/echarts/echarts.min.js"></script>
+    <script src="assets/vendor/quill/quill.js"></script>
+    <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>-->
+    <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+
+    <!--<script src="assets/vendor/php-email-form/validate.js"></script> -->
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
